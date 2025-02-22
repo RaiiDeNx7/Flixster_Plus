a8c95e0e430a09cb2b99b5ee208950dab1e4854d
Hunter Smith <huntertsmith4778@gmail.com>
2025-02-21 21:46:38 -0500

    Popular people are now side by side

--- app/src/main/java/com/example/flixster/MovieFragment.kt
+++ app/src/main/java/com/example/flixster/MovieFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.*
@@ -32,13 +33,17 @@ class MovieFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
         recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Set the LayoutManager to GridLayoutManager with 2 columns
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+
         fetchPeople() // Fetch popular people
         return view
     }
@@ -73,3 +78,4 @@ class MovieFragment : Fragment() {
 
     data class PersonResponse(val results: List<PersonClass>)
 }
+
