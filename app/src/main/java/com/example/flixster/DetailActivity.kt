acd8d83d82e2d96b4e48dd3166f72105ea89e85c
Hunter Smith <huntertsmith4778@gmail.com>
2025-02-22 02:44:05 -0500

    Finished Project. The display is centered nicely now.

--- app/src/main/java/com/example/flixster/DetailActivity.kt
+++ app/src/main/java/com/example/flixster/DetailActivity.kt
@@ -14,48 +14,48 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.detail_activity)
 
         // Retrieve data from intent
-        val movieName = intent.getStringExtra("PERSON_NAME")
-        val movieImage = intent.getStringExtra("PERSON_IMAGE")
+        val personName = intent.getStringExtra("PERSON_NAME")
+        val personImage = intent.getStringExtra("PERSON_IMAGE")
 
         // Find views
         val titleTextView = findViewById<TextView>(R.id.person_title)
         val imageView = findViewById<ImageView>(R.id.person_image)
 
         // Set data to views
-        titleTextView.text = movieName
+        titleTextView.text = personName
+
+        // Load profile image
         Glide.with(this)
-            .load("https://image.tmdb.org/t/p/w500/$movieImage")
+            .load("https://image.tmdb.org/t/p/w500/$personImage")
             .centerInside()
             .into(imageView)
 
         // Retrieve the ArrayList<KnownFor> from the intent
         val knownForList = intent.getParcelableArrayListExtra<KnownFor>("KNOWN_FOR_LIST")
 
-        // Find views for best works
+        // Find views for "Known For" movie
         val movieTitle = findViewById<TextView>(R.id.movie_title)
         val overview = findViewById<TextView>(R.id.overview)
         val posterImage = findViewById<ImageView>(R.id.movie_poster)
 
         // Check if the knownForList is not null and has at least one item
-        knownForList?.let {
-            if (it.isNotEmpty()) {
-                val knownForFirstItem = it[0] // Get the first item in the list
+        if (!knownForList.isNullOrEmpty()) {
+            // Get the first movie in the knownFor list
+            val knownForFirstItem = knownForList[0]
 
-                // Set the details for the first "Known For" movie/work
-                movieTitle.text = knownForFirstItem.title
-                overview.text = knownForFirstItem.overview
+            // Set the movie title
+            movieTitle.text = "Known For: ${knownForFirstItem.title}"
 
-                Glide.with(this)
-                    .load("https://image.tmdb.org/t/p/w500/${knownForFirstItem.posterPath}")
-                    .centerInside()
-                    .into(posterImage)
-            } else {
-                Log.e("DetailActivity", "KnownFor list is empty")
-            }
-        } ?: run {
-            Log.e("DetailActivity", "KnownFor list is null")
+            // Set the movie overview
+            overview.text = knownForFirstItem.overview
+
+            // Load movie poster
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w500/${knownForFirstItem.posterPath}")
+                .centerInside()
+                .into(posterImage)
+        } else {
+            Log.e("DetailActivity", "KnownFor list is either null or empty")
         }
-
-
     }
 }
